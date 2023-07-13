f2199748124617158c98506d855028a01f20d3fb
gierdiaz <gierdiaz@hotmail.com>
2023-07-13 18:57:18 -0300

    construct na classe pai e filha usando parent

--- public/index.php
+++ public/index.php
@@ -31,6 +31,27 @@ $abajur->isOn = true;
 echo json_encode($abajur->abajur());
  */
 
-$user = new User;
 
-echo $user->info();
+//construct na classe pai e filha
+class User1 {
+    public function __construct(public string $name, public int $age)
+    {
+
+    }
+
+    public function getUserInfo(){
+        return "name: {$this->name} | age: {$this->age}";
+    }
+}
+
+
+class User2 extends User1 {
+    public function __construct(string $name, int $age)
+    {
+        parent::__construct($name, $age);
+    }
+}
+
+$user = new User2('name', 0);
+
+echo $user->getUserInfo();
